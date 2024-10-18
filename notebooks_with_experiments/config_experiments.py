996e0328546b30b99c51ce3d5f9092e89ff86b08
Agata Kaczmarek <ak.agata.kaczmarek@gmail.com>
2024-10-18 11:16:45 +0200

    graph and confidence notebooks

--- notebooks_with_experiments/config_experiments.py
+++ notebooks_with_experiments/config_experiments.py
@@ -2,6 +2,7 @@ OPEN_AI_KEY="sk-proj-SmuTm3IWYSi8Fyg_lHRgfDu0kQsZAD7UZMiMPGSsxLODPbkTFgi-7R8DM7T
 
 PATH_ORIGINAL_DATA="../data/Articles_2023.json"
 PATH_GT_DATA="../data/articles_categories_their_matched_companies.csv"
+PATH_MY_GT="../data/articles_categories_my_gt_2.csv"
 PATH_NOT_PREPROCESSED="../data/chosen_articles.txt"
 PATH_4O="../data/chosen_articles_cleaned_4o.txt"
 PATH_REGEX="../data/chosen_articles_cleaned_regex.txt"
@@ -9,3 +10,6 @@ PATH_HUMAN="../data/chosen_articles_cleaned_by_me.txt"
 
 PATH_TO_SAVE_PREPROCESSED="../data/"
 PATH_TO_SAVE_RESULTS="results/"
+
+PATH_TWITTER_DATA="../data/tweet-sentiment-extraction/test.csv"
+PATH_NEWS_DATA="../data/news_sentiment_dataset/Sentiment_dataset.csv"
