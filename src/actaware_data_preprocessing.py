cf3023050c1cdb9851bc0c639aff33541aacbf71
Agata Kaczmarek <ak.agata.kaczmarek@gmail.com>
2024-11-23 21:31:34 +0100

    first update for paper

--- src/actaware_data_preprocessing.py
+++ src/actaware_data_preprocessing.py
@@ -3,7 +3,8 @@ import pandas as pd
 import emoji
 import copy
 from src.config import (
-  PATH_ORIGINAL_DATA
+  PATH_ORIGINAL_DATA,
+  LIST_OF_REGEX
 )
 
 def load_original_data(path_to_data=PATH_ORIGINAL_DATA, first_rows=100):
@@ -26,52 +27,65 @@ def load_original_data(path_to_data=PATH_ORIGINAL_DATA, first_rows=100):
 
   #regex cleaning
   #replace artefacts in ContentRaw with ''
-  to_replace1='Get the free Morning Headlines email for news from our reporters across the world'
-  to_replace2='Sign up to our free Morning Headlines'
-  to_replace3='email Please enter a valid email address Please enter a valid email address SIGN UP'
-  to_replace4='I would like to be emailed about offers, events and updates from The Independent.'
-  to_replace5='Read our privacy notice'
-  to_replace6='Thanks for signing up to the Morning Headlines email'
-  to_replace7='{{ #verifyErrors }}'
-  to_replace8='{{ message }}'
-  to_replace9='{{ /verifyErrors }}'
-  to_replace10='{{ ^verifyErrors }}'
-  to_replace11='Something went wrong. Please try again later'
-  to_replace12=r'Advertisement !- - ad:.*?- -> '
-  to_replace13=r'<!- - ad:.*?- ->'
-  to_replace14=r'<a href=.*?</a>'
-  to_replace15=r'http.*? '
-  to_replace16='CLICK HERE TO SUBSCRIBE TO FOX NATION'
-  to_replace17='CLICK HERE FOR MORE SPORTS COVERAGE ON FOXNEWS.COM'
-  to_replace18='CLICK HERE TO GET THE FOX NEWS APP'
-  to_replace19='CREATE FREE ACCOUNT'
-  to_replace20='CLICK HERE TO DONATE TO "MAKE CAMO YOUR CAUSE"'
-  to_replace21='CLICK HERE FOR MORE SPORTS COVERAGE ON FOXNEWS.COM'
-  to_replace22='subscribe to my free CyberGuy Report Newsletter by heading to Cyberguy.com/Newsletter CLICK HERE'
 
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace1, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace2, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace3, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace4, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace5, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace6, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace7, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace8, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace9, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace10, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace11, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace12, '', regex=True)
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace13, '', regex=True)
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace14, '', regex=True)
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace15, '', regex=True)
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace16, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace17, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace18, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace19, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace20, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace21, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace22, '')
-  data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(r'\s+', ' ', regex=True)
+  # to_replace1='Get the free Morning Headlines email for news from our reporters across the world'
+  # to_replace2='Sign up to our free Morning Headlines'
+  # to_replace3='email Please enter a valid email address Please enter a valid email address SIGN UP'
+  # to_replace4='I would like to be emailed about offers, events and updates from The Independent.'
+  # to_replace5='Read our privacy notice'
+  # to_replace6='Thanks for signing up to the Morning Headlines email'
+  # to_replace7='{{ #verifyErrors }}'
+  # to_replace8='{{ message }}'
+  # to_replace9='{{ /verifyErrors }}'
+  # to_replace10='{{ ^verifyErrors }}'
+  # to_replace11='Something went wrong. Please try again later'
+  # to_replace12=r'Advertisement !- - ad:.*?- -> '
+  # to_replace13=r'<!- - ad:.*?- ->'
+  # to_replace14=r'<a href=.*?</a>'
+  # to_replace15=r'http.*? '
+  # to_replace16='CLICK HERE TO SUBSCRIBE TO FOX NATION'
+  # to_replace17='CLICK HERE FOR MORE SPORTS COVERAGE ON FOXNEWS.COM'
+  # to_replace18='CLICK HERE TO GET THE FOX NEWS APP'
+  # to_replace19='CREATE FREE ACCOUNT'
+  # to_replace20='CLICK HERE TO DONATE TO "MAKE CAMO YOUR CAUSE"'
+  # to_replace21='CLICK HERE FOR MORE SPORTS COVERAGE ON FOXNEWS.COM'
+  # to_replace22='subscribe to my free CyberGuy Report Newsletter by heading to Cyberguy.com/Newsletter CLICK HERE'
+
+  # List of patterns to replace with ''
+  to_replace = LIST_OF_REGEX
+
+  # Replace patterns in 'ContentRaw'
+  for pattern in to_replace:
+      data_df_small_all['ContentRaw'] = data_df_small_all['ContentRaw'].str.replace(pattern, '', regex=bool('regex' in pattern))
+
+  # Normalize whitespace
+  data_df_small_all['ContentRaw'] = data_df_small_all['ContentRaw'].str.replace(r'\s+', ' ', regex=True)
+
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace1, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace2, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace3, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace4, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace5, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace6, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace7, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace8, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace9, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace10, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace11, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace12, '', regex=True)
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace13, '', regex=True)
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace14, '', regex=True)
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace15, '', regex=True)
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace16, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace17, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace18, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace19, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace20, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace21, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(to_replace22, '')
+  # data_df_small_all['ContentRaw']=data_df_small_all['ContentRaw'].str.replace(r'\s+', ' ', regex=True)
+
+  
 
   # remove records which have `None` values in either TitleRaw or ContentRaw
   index_with_none=data_df_small_all[data_df_small_all['ContentRaw'].isnull() + data_df_small_all['TitleRaw'].isnull()].index
--- src/config.py
+++ src/config.py
@@ -8,3 +8,28 @@ PATH_REGEX="data/chosen_articles_cleaned_regex.txt"
 PATH_HUMAN="data/chosen_articles_cleaned_by_me.txt"
 
 PATH_TO_SAVE_PREPROCESSED="data/"
+
+LIST_OF_REGEX=[
+      'Get the free Morning Headlines email for news from our reporters across the world',
+      'Sign up to our free Morning Headlines',
+      'email Please enter a valid email address Please enter a valid email address SIGN UP',
+      'I would like to be emailed about offers, events and updates from The Independent.',
+      'Read our privacy notice',
+      'Thanks for signing up to the Morning Headlines email',
+      '{{ #verifyErrors }}',
+      '{{ message }}',
+      '{{ /verifyErrors }}',
+      '{{ ^verifyErrors }}',
+      'Something went wrong. Please try again later',
+      r'Advertisement !- - ad:.*?- -> ',
+      r'<!- - ad:.*?- ->',
+      r'<a href=.*?</a>',
+      r'http.*? ',
+      'CLICK HERE TO SUBSCRIBE TO FOX NATION',
+      'CLICK HERE FOR MORE SPORTS COVERAGE ON FOXNEWS.COM',
+      'CLICK HERE TO GET THE FOX NEWS APP',
+      'CREATE FREE ACCOUNT',
+      'CLICK HERE TO DONATE TO "MAKE CAMO YOUR CAUSE"',
+      'subscribe to my free CyberGuy Report Newsletter by heading to Cyberguy.com/Newsletter CLICK HERE'
+      
+  ]
